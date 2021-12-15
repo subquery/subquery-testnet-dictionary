027be75c5b5c7f4872095d5001d26bb7041ca3c1
Ian He <39037239+ianhe8x@users.noreply.github.com>
2021-12-16 11:07:48 +1300

    parallel handle everything (#4)

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,7 +1,7 @@
 import {EventRecord, EvmLog} from "@polkadot/types/interfaces"
-import {SubstrateExtrinsic,SubstrateEvent,SubstrateBlock} from "@subql/types";
+import {SubstrateExtrinsic,SubstrateBlock} from "@subql/types";
 import { SpecVersion, Event, Extrinsic, EvmLog as EvmLogModel, EvmTransaction } from "../types";
-import { MoonbeamCall } from "@subql/contract-processors/dist/moonbeam";
+import MoonbeamDatasourcePlugin, { MoonbeamCall } from "@subql/contract-processors/dist/moonbeam";
 import { inputToFunctionSighash, isZero } from "../utils";
 import { wrapExtrinsics } from "./utils";
 
@@ -16,37 +16,45 @@ export async function handleBlock(block: SubstrateBlock): Promise<void> {
         specVersion.blockHeight = block.block.header.number.toBigInt();
         await specVersion.save();
     }
-
-    await Promise.all(block.events.map((evt, idx)=>handleEvent(block.block.header.number.toString(), idx, evt)));
-
-    await Promise.all(wrapExtrinsics(block).map(handleCall));
+    const eventData = block.events.map((evt, idx)=>handleEvent(block.block.header.number.toString(), idx, evt));
+    const events = eventData.map(([evt])=>evt);
+    const logs = eventData.map(([_,log])=>log).filter(log=>log);
+    const calls = wrapExtrinsics(block).map((ext,idx)=>handleCall(`${block.block.header.number.toString()}-${idx}`,ext));
+    //baseFilter: [{module: 'ethereum', method: 'transact'}],
+    const evmCalls: MoonbeamCall[] = await Promise.all(wrapExtrinsics(block).filter(ext => ext.extrinsic.method.section === 'ethereum' && ext.extrinsic.method.method === 'transact').map( (ext) => MoonbeamDatasourcePlugin.handlerProcessors['substrate/MoonbeamCall'].transformer(ext, {} as any, undefined, undefined))) as any;
+    await Promise.all([
+            ...events.map(event=>event.save()),
+            ...logs.map(log=>log.save()),
+            ...calls.map(call=>call.save()),
+            ...evmCalls.map(call=>handleEvmTransaction(call).save()),
+    ]);
 }
 
-export async function handleEvent(blockNumber: string, eventIdx: number, event: EventRecord): Promise<void> {
+export function handleEvent(blockNumber: string, eventIdx: number, event: EventRecord): [Event, EvmLogModel] {
     const newEvent = new Event(`${blockNumber}-${eventIdx}`);
     newEvent.blockHeight = BigInt(blockNumber);
     newEvent.module = event.event.section;
     newEvent.event = event.event.method;
-    const p = [newEvent.save()];
+    const ret: [Event, EvmLogModel] = [newEvent, undefined];
     if (event.event.section === 'evm' && event.event.method === 'Log') {
-        p.push(handleEvmEvent(blockNumber, eventIdx, event));
+        ret[1] = handleEvmEvent(blockNumber, eventIdx, event);
     }
-    await Promise.all(p);
+    return ret;
 }
 
-export async function handleCall(extrinsic: SubstrateExtrinsic): Promise<void> {
-    const newExtrinsic = new Extrinsic(extrinsic.extrinsic.hash.toString());
+export function handleCall(idx: string, extrinsic: SubstrateExtrinsic): Extrinsic {
+    const newExtrinsic = new Extrinsic(idx);
     newExtrinsic.module = extrinsic.extrinsic.method.section;
     newExtrinsic.call = extrinsic.extrinsic.method.method;
     newExtrinsic.blockHeight = extrinsic.block.block.header.number.toBigInt();
     newExtrinsic.success = extrinsic.success;
     newExtrinsic.isSigned = extrinsic.extrinsic.isSigned;
-    await newExtrinsic.save();
+    return newExtrinsic;
 }
 
-async function handleEvmEvent(blockNumber: string, eventIdx: number, event: EventRecord): Promise<void> {
+function handleEvmEvent(blockNumber: string, eventIdx: number, event: EventRecord): EvmLogModel {
     const [{address, data, topics}] = event.event.data as unknown as [EvmLog];
-    const log = EvmLogModel.create({
+    return EvmLogModel.create({
         id: `${blockNumber}-${eventIdx}`,
         address: address.toString(),
         blockHeight: BigInt(blockNumber),
@@ -55,15 +63,14 @@ async function handleEvmEvent(blockNumber: string, eventIdx: number, event: Even
         topics2: topics[2]?.toHex(),
         topics3: topics[3]?.toHex(),
     });
-    await log.save();
 }
 
-export async function handleEvmTransaction(tx: MoonbeamCall): Promise<void> {
+export function handleEvmTransaction(tx: MoonbeamCall): EvmTransaction {
     if (!tx.hash) {
         return;
     }
     const func = isZero(tx.data) ? undefined : inputToFunctionSighash(tx.data);
-    const transaction = EvmTransaction.create({
+    return EvmTransaction.create({
         id: tx.hash,
         from: tx.from,
         to: tx.to,
@@ -71,5 +78,4 @@ export async function handleEvmTransaction(tx: MoonbeamCall): Promise<void> {
         blockHeight: BigInt(tx.blockNumber.toString()),
         success: tx.success,
     });
-    await transaction.save();
 }
