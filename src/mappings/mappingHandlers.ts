a72363203aa41834db70807429933bc54fc40203
Ian He <39037239+ianhe8x@users.noreply.github.com>
2021-12-29 23:42:05 +1300

    Use bulk create (#5)
    * use BulkCreate
    
    * upgrade dependencies & use chaintype.ts

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,5 +1,5 @@
 import {EventRecord, EvmLog} from "@polkadot/types/interfaces"
-import {SubstrateExtrinsic,SubstrateBlock} from "@subql/types";
+import {SubstrateExtrinsic,SubstrateEvent,SubstrateBlock} from "@subql/types";
 import { SpecVersion, Event, Extrinsic, EvmLog as EvmLogModel, EvmTransaction } from "../types";
 import MoonbeamDatasourcePlugin, { MoonbeamCall } from "@subql/contract-processors/dist/moonbeam";
 import { inputToFunctionSighash, isZero } from "../utils";
@@ -23,11 +23,14 @@ export async function handleBlock(block: SubstrateBlock): Promise<void> {
     //baseFilter: [{module: 'ethereum', method: 'transact'}],
     const evmCalls: MoonbeamCall[] = await Promise.all(wrapExtrinsics(block).filter(ext => ext.extrinsic.method.section === 'ethereum' && ext.extrinsic.method.method === 'transact').map( (ext) => MoonbeamDatasourcePlugin.handlerProcessors['substrate/MoonbeamCall'].transformer(ext, {} as any, undefined, undefined))) as any;
     await Promise.all([
-            ...events.map(event=>event.save()),
-            ...logs.map(log=>log.save()),
-            ...calls.map(call=>call.save()),
-            ...evmCalls.map(call=>handleEvmTransaction(call).save()),
+        (store as any).bulkCreate('Event', events),
+        (store as any).bulkCreate('EvmLog', logs),
+        (store as any).bulkCreate('Extrinsic', calls),
+        (store as any).bulkCreate('EvmTransaction', evmCalls.map(handleEvmTransaction)),
     ]);
+
+    // await Promise.all(block.events.map((evt, idx)=>handleEvent(block.block.header.number.toString(), idx, evt)));
+
 }
 
 export function handleEvent(blockNumber: string, eventIdx: number, event: EventRecord): [Event, EvmLogModel] {
@@ -54,7 +57,7 @@ export function handleCall(idx: string, extrinsic: SubstrateExtrinsic): Extrinsi
 
 function handleEvmEvent(blockNumber: string, eventIdx: number, event: EventRecord): EvmLogModel {
     const [{address, data, topics}] = event.event.data as unknown as [EvmLog];
-    return EvmLogModel.create({
+    const log = EvmLogModel.create({
         id: `${blockNumber}-${eventIdx}`,
         address: address.toString(),
         blockHeight: BigInt(blockNumber),
@@ -63,6 +66,7 @@ function handleEvmEvent(blockNumber: string, eventIdx: number, event: EventRecor
         topics2: topics[2]?.toHex(),
         topics3: topics[3]?.toHex(),
     });
+    return log;
 }
 
 export function handleEvmTransaction(tx: MoonbeamCall): EvmTransaction {
@@ -70,7 +74,7 @@ export function handleEvmTransaction(tx: MoonbeamCall): EvmTransaction {
         return;
     }
     const func = isZero(tx.data) ? undefined : inputToFunctionSighash(tx.data);
-    return EvmTransaction.create({
+    const transaction = EvmTransaction.create({
         id: tx.hash,
         from: tx.from,
         to: tx.to,
@@ -78,4 +82,5 @@ export function handleEvmTransaction(tx: MoonbeamCall): EvmTransaction {
         blockHeight: BigInt(tx.blockNumber.toString()),
         success: tx.success,
     });
+    return transaction;
 }
--- typegen.js
+++
@@ -1,3 +0,0 @@
-const fs = require('fs');
-const { typesBundlePre900 } = require('moonbeam-types-bundle');
-fs.writeFileSync('chaintypes.json', JSON.stringify({typesBundle: typesBundlePre900}, undefined, 2));
